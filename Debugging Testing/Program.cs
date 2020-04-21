01aeeee401804684bcaf9661f21d2268197308d5
SethKaplan <drpepperjazz@yahoo.com>
2020-04-21 18:31:56 -0400

    Add project files.

---
+++ Debugging Testing/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Debugging_Testing {
+    class Program {
+        static void Main(string[] args) {
+            var friends = new List<string> { "Dave", "Ian", "Bondar", "Ben", "Jay", "Chris B", "George", "JC" };
+            var partyFriends = GetPartyFriends(friends, 3);
+            foreach(var f in partyFriends) {
+                Console.WriteLine(f);
+            }
+        }
+        public static List<string> GetPartyFriends(List<string> list, int count) {
+            var partyFriends = new List<string>();
+            while (partyFriends.Count < count) {
+                var currentFriend = GetPartyFriend(list);
+                partyFriends.Add(currentFriend);
+                list.Remove(currentFriend);
+            }
+            return partyFriends;
+        }
+        public static string GetPartyFriend(List<string> list) {
+
+            string shortestName = list[0];
+            for (var i = 0; i < list.Count; i++) {
+                if (list[i].Length > shortestName.Length) {
+                    shortestName = list[i];
+                }
+            }
+            return shortestName;
+        }
+    
+    }
+}
